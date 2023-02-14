1a24bf49cfd6de9f6f5734c0edb7a7186716de30
Yash Pandey <pandeyyash7052@gmail.com>
2023-02-15 01:57:18 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//  importing the Custom Components.
+import Header from "./components/Header"
+import CustomForm from "./components/CustomForm"
+
+
+function App() {
+  
+  return (
+    <div className="container flex flex-col justify-center items-center ">
+      <Header />
+      <CustomForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CustomForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomForm = () => {
+  return (
+    <form className="to_do" onSubmit={(e)=>{e.preventDefault()}}>
+        
+    </form>
+  )
+}
+
+export default CustomForm
